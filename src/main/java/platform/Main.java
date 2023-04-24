4a7a191c22b52b0580cc4d5b92bb76158f4398e6
Jonas Gedeshi <jgedeshi@AEXIS-MEDICAL.AD>
2023-04-24 15:32:05 +0200

    key- and mouselisteners

--- src/main/java/platform/Main.java
+++ src/main/java/platform/Main.java
@@ -1,17 +1,10 @@
 package platform;
 
-/**
- * <p>Title        : ${NAME} </p>
- * <p>Description  : </p>
- *
- * <p>Company      : Aexis Medical BV </p>
- * <p>Copyright    : ©Aexis Medical - April 2023 - All rights reserved </p>
- *
- * @author Jonas Gedeshi
- * @version 24/04/2023 - Jonas - created the class
- */
+import engine.Window;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Window window = Window.get();
+        window.run();
     }
 }
