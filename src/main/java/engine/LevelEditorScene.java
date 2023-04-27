e84c5d6ffced4cd47c9ad7fea06373500df64006
Jonas Gedeshi <jgedeshi@AEXIS-MEDICAL.AD>
2023-04-27 11:31:05 +0200

    resourcemanagement

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -3,6 +3,9 @@ package engine;
 import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
+import util.AssetPool;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 public class LevelEditorScene extends Scene {
 
@@ -12,7 +15,7 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
-        this.camera = new Camera(new Vector2f());
+        this.camera = new Camera(new Vector2f(-250, 0));
 
         int xOffset = 10;
         int yOffset = 10;
@@ -21,21 +24,37 @@ public class LevelEditorScene extends Scene {
         float totalHeight = (float)(300 - yOffset * 2);
         float sizeX = totalWidth / 100.0f;
         float sizeY = totalHeight / 100.0f;
+        float padding = 0;
 
         for (int x = 0; x < 100; x++) {
             for (int y = 0; y < 100; y++) {
-                float xPos = xOffset + (x * sizeX);
-                float yPos = yOffset + (y * sizeY);
+                float xPos = xOffset + (x * sizeX) + (padding * x);
+                float yPos = yOffset + (y * sizeY) + (padding * y);
 
                 GameObject go = new GameObject("obj" + x + ""+ y, new Transform(new Vector2f(xPos, yPos), new Vector2f(sizeX, sizeY)));
                 go.addComponent(new SpriteRenderer(new Vector4f(xPos / totalWidth, yPos / totalHeight, 1, 1)));
                 this.addGameObjectToScene(go);
             }
         }
+
+        loadResources();
+    }
+
+    private void loadResources(){
+        AssetPool.getShader("assets/shaders/default.glsl");
     }
 
     @Override
     public void update(float dt) {
+        if (KeyListener.isKeyPressed(GLFW_KEY_RIGHT))
+            camera.position.x += 100f * dt;
+        if (KeyListener.isKeyPressed(GLFW_KEY_LEFT))
+            camera.position.x -= 100f * dt;
+        if (KeyListener.isKeyPressed(GLFW_KEY_UP))
+            camera.position.y += 100f * dt;
+        if (KeyListener.isKeyPressed(GLFW_KEY_DOWN))
+            camera.position.y -= 100f * dt;
+
 
         for (GameObject go : this.gameObjects) {
             go.update(dt);
