d4d3be11fdb68f3de4535acfecc6c349b43ca760
Jonas Gedeshi <jgedeshi@AEXIS-MEDICAL.AD>
2023-05-09 14:06:31 +0200

    Framebuffers

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -4,6 +4,7 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 import renderer.DebugDraw;
+import renderer.Framebuffer;
 import scenes.LevelEditorScene;
 import scenes.LevelScene;
 import scenes.Scene;
@@ -18,6 +19,7 @@ public class Window {
     private String title;
     private long glfwWindow;
     private ImGuiLayer imGuiLayer;
+    private Framebuffer framebuffer;
 
     public float r, g, b, a;
 
@@ -26,8 +28,8 @@ public class Window {
     private static Window window = null;
 
     private Window() {
-        this.width = 1280;
-        this.height = 720;
+        this.width = 1920;
+        this.height = 1080;
         this.title = "Jumper";
         r = 0.9f;
         g = 0.9f;
@@ -126,6 +128,8 @@ public class Window {
         this.imGuiLayer = new ImGuiLayer(glfwWindow);
         this.imGuiLayer.initImGui();
 
+        this.framebuffer = new Framebuffer(1920,1080);
+
         Window.changeScene(0);
     }
 
@@ -143,10 +147,12 @@ public class Window {
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            this.framebuffer.bind();
             if (dt >= 0) {
                 DebugDraw.draw();
                 currentScene.update(dt);
             }
+            this.framebuffer.unbind();
 
             this.imGuiLayer.update(dt, currentScene);
             glfwSwapBuffers(glfwWindow);
